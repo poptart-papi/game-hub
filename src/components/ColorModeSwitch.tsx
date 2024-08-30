279791c3145de9a395909e5904f04900051168f2
Poptart Papi <papibpoppin@gmail.com>
2024-08-30 16:45:46 -0400

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -4,13 +4,13 @@ const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Switch
         colorScheme="green"
         isChecked={colorMode === 'dark'}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
